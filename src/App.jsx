96c616c697c643eeb8cee9fbf3e104f0c893abe9
Bharath136 <bk3604073@gmail.com>
2026-03-17 23:05:09 +0530

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            {/* <Route path="/services" element={<Services />} />
-            <Route path="/products" element={<Products />} />
+            <Route path="/services" element={<Services />} />
+            {/* <Route path="/products" element={<Products />} />
             <Route path="/sustainability" element={<Sustainability />} /> */}
             <Route path="/contact" element={<Contact />} />
             {/* <Route path="/support" element={<Support />} /> */}
