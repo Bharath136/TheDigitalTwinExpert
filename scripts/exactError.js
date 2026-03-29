c6fcf9388bbd746427930f8ee19cbf8a9e491aaf
Anil Kumar Desai <anilkumardesai18@gmail..com>
2026-03-29 13:12:38 +0530

    new updates

--- scripts/exactError.js
+++
@@ -1,8 +0,0 @@
-import { exec } from 'child_process';
-exec('npm run build', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-  }
-  console.log(`STDOUT: ${stdout}`);
-  console.log(`STDERR: ${stderr}`);
-});
--- scripts/readLog.js
+++
@@ -1,3 +0,0 @@
-import fs from 'fs';
-const data = fs.readFileSync('build-log.txt', 'utf16le');
-console.log(data);
